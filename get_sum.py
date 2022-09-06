816608949bce6145394009bd1edf215f5eac65a8
dima0046 <dima0046@gmail.com>
2022-09-06 14:44:26 +0300

    Добавил файл для примера работы с git

---
+++ get_sum.py
@@ -0,0 +1,4 @@
+def get_sum(one, two, delimiter='&'):
+    return print(one.capitalize() + delimiter + two.capitalize())
+
+get_sum("learn", "python")
---
+++ price.py
@@ -0,0 +1,6 @@
+def format_price(price):
+    cena = int(price)
+    str1 = f"price: {cena} rub."
+    return print(str1)
+
+format_price(56.24)
